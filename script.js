f2484a0318c5c187e88c1640cf7f3ef3adecf93c
DenAustin <uduogu@gmail.com>
2020-02-04 00:37:20 +0200

    firt commit

---
+++ script.js
@@ -0,0 +1,34 @@
+
+const flexContainer = document.querySelector(".countries-container")
+const url = "https://restcountries.eu/rest/v2/all"
+fetch(url)
+    .then(response => response.json())
+    .then(countries => {
+        for (const country of countries) {
+            console.log(country)
+            let {
+                name,
+                capital,
+                population,
+                region,
+                languages,
+                flag,
+                topLevelDomain: domain
+            } = country
+            let div = document.createElement("div")
+            let countryName = document.createElement("h2")
+            let countryCapital = document.createElement("p")
+            let countryPopulation = document.createElement("p")
+            let countryflag = document.createElement("img")
+            countryName.textContent = name
+            countryCapital.textContent = capital
+            countryPopulation.textContent = population
+            countryflag.src = flag
+            div.appendChild(countryName)
+            div.appendChild(countryCapital)
+            div.appendChild(countryPopulation)
+            div.appendChild(countryflag)
+            flexContainer.appendChild(div)
+            console.log(name, capital, population, region, languages, domain[0])
+        }
+    })
